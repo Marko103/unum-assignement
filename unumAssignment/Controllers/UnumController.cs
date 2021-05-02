f6547d82e6cdc34c432fd2b92e7a08696790a279
Marko Martinović <marko.martinovic1207@outlook.com>
2021-05-02 22:55:44 +0200

    BE

---
+++ unumAssignment/Controllers/UnumController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using unumAssignment.Model;
+
+namespace unumAssignment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UnumController : ControllerBase
+    {
+        [HttpPost]
+        [Route("calculate")]
+        public UnumResponse CalculateValues([FromBody] IEnumerable<int> unumRequest)
+        {
+            return new UnumResponse
+            {
+                Numbers = string.Join(", ", unumRequest),
+                Max = Enumerable.Max(unumRequest),
+                Min = Enumerable.Min(unumRequest),
+                Avg = Math.Round(Enumerable.Average(unumRequest), 2)
+            };
+        }
+    }
+}
---
+++ unumAssignment/Model/UnumResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace unumAssignment.Model
+{
+    public class UnumResponse
+    {
+        public string Numbers { get; set; }
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public double Avg { get; set; }
+    }
+}
